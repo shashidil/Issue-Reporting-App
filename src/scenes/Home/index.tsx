557a851ce6966c012911289d4d926745e37ce79c
roxdi <roxdil426@gmail.com>
2023-09-04 21:36:11 +0530

    fixed bugs

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useCallback} from 'react'
-import {Space, Table, Button, Modal, Form, Select, Input, SelectProps} from 'antd';
+import {Space, Table, Button, Modal, Form, Select, Input, notification,Col, Row} from 'antd';
 import type {ColumnsType} from 'antd/es/table';
 import axios from 'axios';
 
@@ -14,16 +14,13 @@ interface DataType {
     };
 };
 
+let issueId!: any;
+
 export default function Home() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [issueList, setIssueList] = useState<DataType[]>([]);
     const [formData, setFormData] = useState({title: '', description: '', status: {id: '', name: ''}});
-    const [editingIssue, setEditingIssue] = useState({
-        issueId:'',
-        title: '',
-        description: '',
-        status: {id: '', name: ''}
-    });
+    const [editingIssue, setEditingIssue] = useState<any>({});
     const [originalStatus, setOriginalStatus] = useState<number | null>(null);
     const [isUpdate, setIsUpdate] = useState(false);
     const [statusOptions, setStatusOptions] = useState<{ id: number; name: string }[]>([]);
@@ -31,19 +28,12 @@ export default function Home() {
     const [isSearch, setIsSearch] = useState(false);
     const FormItem = Form.Item;
     const [form] = Form.useForm();
-
     const resetFormData = () => {
         setFormData({title: '', description: '', status: {id: '', name: ''}});
     };
     const showModal = (isUpdate:boolean) => {
         setIsUpdate(isUpdate);
         setIsModalOpen(true);
-        setEditingIssue({issueId: '', title: '', description: '', status: {id: '', name: ''}});
-    };
-
-    const handleOk = () => {
-        setIsModalOpen(false);
-        form.submit();
     };
 
     const handleCancel = () => {
@@ -71,7 +61,7 @@ export default function Home() {
             render: (_, record) => record.status.name,
         },
         {
-            title: '',
+            title: 'Action',
             dataIndex: 'update',
             key: 'update',
             render: (_, record) => (
@@ -104,6 +94,7 @@ export default function Home() {
     }, []);
 
     const handleFormSubmit = useCallback((values: any) => {
+        console.log(values);
         try {
             const getStatusName = (statusValue: string) => {
                 switch (statusValue) {
@@ -127,44 +118,62 @@ export default function Home() {
                 }
             };
 
-            if (null == editingIssue) {
+            if (!issueId ) {
                 axios.post('http://localhost:8080/api/issue-report/new-issue', issuedto)
                     .then((response) => {
                         if (response.status === 201) {
                             fetchData();
-                            console.log('Data sent successfully');
+                            notification.success({
+                                message: 'Success',
+                                description: 'Data sent successfully',
+                            });
                         } else {
-                            console.error('Failed to send data to the backend');
+                            notification.error({
+                                message: 'Error',
+                                description: 'Failed to send data to the backend',
+                            });
                         }
                     })
                     .catch((error) => {
-                        console.error('Error sending data to the backend', error);
+                        notification.error({
+                            message: 'Error',
+                            description: 'Error sending data to the backend',
+                        });
                     });
             } else {
-                const baseURL = `http://localhost:8080/api/issue-report/update/${editingIssue.issueId}`;
+                const baseURL = `http://localhost:8080/api/issue-report/update/${issueId}`;
                 console.log(editingIssue.issueId);
-                editingIssue.title = issuedto.title;
-                editingIssue.description = issuedto.description;
-                editingIssue.status.id = issuedto.status.id;
-
                 axios.put(baseURL, issuedto)
                     .then((response) => {
                         if (response.status === 200) {
                             fetchData();
-                            console.log('Data updated successfully');
+                            notification.success({
+                                message: 'Success',
+                                description: 'Data sent successfully',
+                            });
                         } else {
-                            console.error('Failed to update data to the backend');
+                            // notification.error({
+                            //     message: 'Error',
+                            //     description: 'Failed to update data to the backend',
+                            // });
                         }
                     })
                     .catch((error) => {
-                        console.error('Error updating data to the backend', error);
+                        notification.error({
+                            message: 'Error',
+                            description: 'Failed to update data to the backend',
+                        });
                     });
             }
         } catch (error) {
-            console.error('Error sending/updating data to the backend', error);
+            notification.error({
+                message: 'Error',
+                description: 'Error sending/updating data to the backend',
+            });
         } finally {
             setFormData({title: '', description: '', status: {id: '', name: ''}});
             setEditingIssue({issueId: '', title: '', description: '', status: {id: '', name: ''}});
+            showModal(false);
         }
     }, []);
 
@@ -179,16 +188,28 @@ export default function Home() {
                     if (response.status === 204) {
                         const updatedList = issueList.filter((item) => item.issueId !== record.issueId);
                         setIssueList(updatedList);
-                        console.log('Item deleted successfully');
+                        notification.success({
+                            message: 'Success',
+                            description: 'Item deleted successfully',
+                        });
                     } else {
-                        console.error('Failed to delete the item');
+                        notification.error({
+                            message: 'Error',
+                            description: 'Failed to delete the item',
+                        });
                     }
                 }).catch((error) => {
-                    console.error('Error deleting the item', error);
+                    notification.error({
+                        message: 'Error',
+                        description: 'Failed to delete the item',
+                    });
                 });
             },
             onCancel: () => {
-                console.log('Deletion canceled');
+                notification.info({
+                    message: 'Info',
+                    description: 'Deletion canceled',
+                });
             },
         });
     };
@@ -202,33 +223,19 @@ export default function Home() {
     }, []);
 
     const handleUpdateClick = (record: DataType) => {
-        const baseURL = `http://localhost:8080/api/issue-report/${record.issueId}`;
-        axios.get(baseURL)
-            .then((response) => {
-                if (response.status === 200) {
-                    const data = response.data;
-                    const updatedData = {
-                        issueId: data.issueId,
-                        title: data.title,
-                        description: data.description,
-                        status: {
-                            id: data.status.id,
-                            name: data.status.name,
-                        },
-                    };
-                    setFormData(updatedData);
-                    setEditingIssue(data);
-                    showModal(false);
-                    setIsUpdate(true);
-                } else {
-                    console.error('Failed to fetch data by issueId');
-                }
-            })
-            .catch((error) => {
-                console.error('Error fetching data by issueId', error);
-            });
+        issueId = record.issueId;
+        setEditingIssue(record);
+        setOriginalStatus(record.status.id);
+        setFormData({
+            title: record.title,
+            description: record.description,
+            status: {
+                id: record.status.id.toString(),
+                name: record.status.name,
+            },
+        });
+        showModal(true);
     };
-
 
     const fetchStatusOptions = () => {
         axios.get('http://localhost:8080/issue-report/status/list')
@@ -254,7 +261,6 @@ export default function Home() {
     const handleSearch = (values: string[]) => {
         try {
             const commaSeparatedValues = values.join(',');
-            console.log(commaSeparatedValues);
 
             const baseURL = "http://localhost:8080/api/issue-report/search-list";
             axios.get(baseURL, {
@@ -270,85 +276,108 @@ export default function Home() {
     }
 
 
-    const filteredIssueList = issueList.filter((issue) => selectedStatus.includes(issue.status.id.toString()));
+    //const filteredIssueList = issueList.filter((issue) => selectedStatus.includes(issue.status.id.toString()));
+
 
     return (
         <>
-            <Space style={{ width: '100%' }} direction="vertical">
-                <Select
-                    mode="multiple"
-                    allowClear
-                    style={{ width: '100%' }}
-                    placeholder="Please select"
-                    value={selectedStatus}
-                    onChange={handleChange}
-                    options={statusOptions.map((statusOption) => ({
-                        label: statusOption.name,
-                        value: statusOption.id, // Assuming 'id' is a string
-                    }))}
-                />
-            </Space>
-            <Button type="primary" onClick={() => showModal(false)}>
-                Add New Issue
-            </Button>
-            <Modal title="Add New Issue" open={isModalOpen} onCancel={handleCancel} onOk={handleOk}  >
+            <Row>
+                <Col span={12}>
+                    <Space style={{ width: '50%' ,marginLeft:'80%',marginTop:'20px'}} direction="vertical">
+                        <Select
+                            mode="multiple"
+                            allowClear
+                            style={{ width: '100%' }}
+                            placeholder="Please select"
+                            value={selectedStatus}
+                            onChange={handleChange}
+                            options={statusOptions.map((statusOption) => ({
+                                label: statusOption.name,
+                                value: statusOption.id,
+                            }))}
+                        />
+                    </Space>
+                </Col>
+                <Col span={12}>
+                    <Button type="primary" onClick={() => {
+                        showModal(false);
+                        issueId = '';
+                        form.resetFields();
+                    }} style={{ marginLeft: '20px',marginTop:'20px' }}>
+                        Add New Issue
+                    </Button>
+                </Col>
+            </Row>
+
+
+            <Modal title="Add New Issue" open={isModalOpen} onCancel={handleCancel} okButtonProps={{ style: { display: 'none' } }} cancelButtonProps={{ style: { display: 'none' } }} destroyOnClose={true} >
                 <Form
-                    labelCol={{span: 4}}
+                    labelCol={{span: 6}}
                     wrapperCol={{span: 14}}
                     layout="horizontal"
                     style={{maxWidth: 600}}
                     onFinish={handleFormSubmit}
                     form={form}
                 >
-
-                    <Form.Item label="Issue Name" name="title" initialValue={formData.title}>
+                    <Form.Item label="Issue Name" name="title"   initialValue={formData.title} rules={[{ required: true }]}>
                         <Input
                             placeholder="Put a title here"
                             name="title"
                             value={formData.title}
                             onChange={handleInputChange}
-
                         />
                     </Form.Item>
-                    <Form.Item label="Description" name="description" initialValue={formData.description}>
+                    <Form.Item label="Description" name="description" initialValue={formData.description} rules={[{ required: true }]}>
                         <Input
                             placeholder="Enter some description "
                             name="description"
                             value={formData.description}
                             onChange={handleInputChange}
+
                         />
                     </Form.Item>
-                    <Form.Item label="Status" name="status" initialValue={formData.status.name}>
+                    <Form.Item label="Status" name="status" initialValue={formData.status.name} rules={[{ required: true }]}>
                         <Select
                             placeholder="Select a status"
                             value={formData.status}
-                            onChange={(value) => setFormData((prevData) => ({ ...prevData, status: value }))}
+                            onChange={(value) =>
+                                setFormData((prevData) => ({ ...prevData, status: value }))
+                            }
                         >
                             {isUpdate ? (
-                                <Select.Option value="5">Test</Select.Option>
-                            ) : null}
-                            <Select.Option value="1" disabled={isUpdate ? formData.status.id !== "1" : false}>
-                                Open
-                            </Select.Option>
-                            <Select.Option value="2" disabled={isUpdate ? formData.status.id !== "1" && formData.status.id !== "2" : false}>
-                                Progress
-                            </Select.Option>
-                            <Select.Option value="3" disabled={isUpdate ? formData.status.id !== "1" && formData.status.id !== "3" : false}>
-                                Waiting
-                            </Select.Option>
-                            <Select.Option value="4" disabled={isUpdate ? formData.status.id === "4" : false}>
-                                Resolved
-                            </Select.Option>
+                                <>
+                                    <Select.Option value="1" disabled={true}>
+                                        Open
+                                    </Select.Option>
+                                    <Select.Option value="2" disabled={originalStatus === 2 || originalStatus === 4}>
+                                        Progress
+                                    </Select.Option>
+                                    <Select.Option value="3" disabled={originalStatus === 1 || originalStatus === 3 || originalStatus === 4}>
+                                        Waiting
+                                    </Select.Option>
+                                    <Select.Option value="4" disabled={originalStatus === 1 || originalStatus === 4}>
+                                        Resolved
+                                    </Select.Option>
+                                </>
+                            ) : (
+                                <>
+                                    <Select.Option value="1">Open</Select.Option>
+                                    <Select.Option value="2">Progress</Select.Option>
+                                    <Select.Option value="3">Waiting</Select.Option>
+                                    <Select.Option value="4">Resolved</Select.Option>
+                                </>
+                            )}
                         </Select>
                     </Form.Item>
-                    {/*<Form.Item wrapperCol={{offset: 4, span: 14}}>*/}
-                    {/*    <Button type="primary" htmlType="submit">*/}
-                    {/*        Submit*/}
-                    {/*    </Button>*/}
-                    {/*</Form.Item>*/}
+                    <Form.Item wrapperCol={{offset: 4, span: 14}}>
+                    <Button type="primary" htmlType="submit">
+                           Submit
+                    </Button>
+                    </Form.Item>
                 </Form>
             </Modal>
-                    <Table columns={columns} dataSource={issueList}></Table>
+                    <Table columns={columns} dataSource={issueList} style={{ padding: '20px' ,margin:'10px'}}></Table>
+
         </>
     );
 
